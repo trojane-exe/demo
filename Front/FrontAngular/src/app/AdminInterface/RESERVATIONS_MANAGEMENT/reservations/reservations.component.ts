ead478594dc1e4464835fbddd6a0570c17c512d9
trojane-exe <soufianeelbouazzaoui3@gmail.com>
2024-08-05 15:33:50 +0100

    some work on the reservation entity

--- Front/FrontAngular/src/app/AdminInterface/RESERVATIONS_MANAGEMENT/reservations/reservations.component.ts
+++ Front/FrontAngular/src/app/AdminInterface/RESERVATIONS_MANAGEMENT/reservations/reservations.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Reservation } from '../../../models/Reservation.model';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { ReservationService } from '../../../services/AdminServices/Reservations/reservation.service';
+import { __importDefault } from 'tslib';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reservations',
   templateUrl: './reservations.component.html',
   styleUrl: './reservations.component.css'
 })
-export class ReservationsComponent {
+export class ReservationsComponent implements OnInit {
 
+  reservations : Reservation[]=[];
+  constructor(private router :Router,private toast:ToastrService, private reservationService : ReservationService){}
+
+  getAllReservation():void{
+    var button = document.getElementById('toggleStatus');
+    this.reservationService.getAllReservation().subscribe({
+      next : (data)=>{
+        this.reservations = data;
+          
+      }
+    })
+
+  }
+
+  ngOnInit(): void {
+    this.getAllReservation();
+    
+  }
 }
